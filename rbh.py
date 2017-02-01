c8d47651a65176b2ee175a7ab0450b596f7b1908
Bob Zimmermann <robert.zimmermann@univie.ac.at>
2017-02-01 19:01:29 +0100

    initial commit of rbh.py

---
+++ rbh.py
@@ -0,0 +1,227 @@
+#! /usr/bin/env python
+
+import math
+import itertools
+import collections
+import argparse
+
+class Hit(object):
+    def __init__(self,qname,sname,pctid,length,mismatches,ngaps,
+                 qstart,qend,sstart,send,evalue,bitscore,
+                 qcov=0.,qlen=0,slen=0):
+        self.qname = qname
+        self.sname = sname
+        self.pctid = float(pctid)
+        self.length = int(length)
+        self.mismatches = int(mismatches)
+        self.ngaps = int(ngaps)
+        self.qstart = int(qstart)
+        self.qend = int(qend)
+        self.sstart = int(sstart)
+        self.send = int(send)
+        self.evalue = float(evalue)
+        self.bitscore = float(bitscore)
+        self.qcov = float(qcov)
+        self.qlen = int(qlen)
+        self.slen = int(slen)
+    def cmp_evalue(self,other):
+        return cmp(self.evalue,other.evalue)
+    def cmp_bitscore(self,other):
+        return cmp(other.bitscore,self.bitscore)
+    def cmp_sbjct(self,other):
+        return cmp(self.sname,other.sname)
+    def __cmp__(self,other):
+        return self.cmp_evalue(other) or self.cmp_bitscore(other) or \
+                self.cmp_sbjct(other)
+    def nonidentical_tie(self,other):
+        return (self.cmp_evalue(other)  == 0 and 
+                self.cmp_bitscore(other) == 0 and
+                self.cmp_sbjct(other) != 0)
+    def __str__(self):
+        return '\t'.join(
+            [str(x) for x in [
+                self.qname,self.sname,self.pctid,self.length,self.mismatches,
+                self.ngaps,self.qstart,self.qend,self.evalue,self.bitscore,
+                self.qcov]])
+
+class HitGroup(object):
+    def __init__(self):
+        self._hits = []
+        self._winners = []
+        self._dirty = False
+    def add(self,hit):
+        self._hits.append(hit)
+        self._dirty = True
+    def _update(self):
+        if self._dirty:
+            self._hits = list(sorted(self._hits))
+            self._winners = [self._hits[0]] + [
+                h for h in self._hits if h.nonidentical_tie(self._hits[0])]
+        self._dirty = False
+    def _get_hits(self):
+        self._update()
+        return self._hits
+    hits = property(_get_hits)
+    def _get_winners(self):
+        self._update()
+        return self._winners
+
+    winners = property(_get_winners)
+
+class HitPair(object):
+    def __init__(self,hit1,hit2=None):
+        self.hit1 = hit1
+        self.hit2 = hit2
+    def _qname_id(self):
+        return '%s_%s' % (self.hit1.qname,self.hit2.qname)
+    def __hash__(self):
+        return hash(self._qname_id())
+    def __cmp__(self,other):
+        return cmp(self._qname_id(),other._qname_id())
+    def __str__(self):
+        return '\t'.join(str(x) for x in [
+            self.hit1.qname,self.hit2.qname,
+            self.hit1.evalue,self.hit2.evalue,
+            self.hit1.qlen,self.hit2.qlen,
+            self.hit1.length])
+
+class CRBFitting(dict):
+    def check(self,hit):
+        length = min(hit.length,len(self)-1)
+        return self[length] <= CRB.transform_evalue(hit.evalue)
+    
+class CRB(object):
+    MIN_EVALUE=1e-200
+    def __init__(self,rbh):
+        self.fitting1 = self.get_fitting([pair.hit1 for pair in rbh])
+        self.fitting2 = self.get_fitting([pair.hit2 for pair in rbh])
+    
+    @classmethod
+    def transform_evalue(cls,evalue):
+        return -1*math.log(evalue or CRB.MIN_EVALUE,10)
+
+    def get_fitting(self,training_hits):
+        lengths = [h.length for h in training_hits]
+        shortest_hit = min(lengths)
+        longest_hit = max(lengths)
+        evalue_dict = collections.defaultdict(list)
+        for hit in training_hits:
+            evalue_dict[hit.length].append(CRB.transform_evalue(hit.evalue))
+        fitting = CRBFitting()
+        for l in range(shortest_hit,longest_hit+1):
+            flank_len = int(max(l*.1,5))
+            evalues = 0.
+            count = 0
+            for flank in range(-flank_len,flank_len+1):
+                if l+flank in evalue_dict:
+                    evalues += sum(evalue_dict[l+flank])
+                    count += len(evalue_dict[l+flank])
+            if count:
+                evalue_mean = evalues/count
+                fitting[l] = evalue_mean
+            if l-1 in fitting and (
+                l not in fitting or fitting[l] < fitting[l-1]):
+                    fitting[l] = fitting[l-1]
+        for l in range(shortest_hit):
+            fitting[l] = fitting[shortest_hit]
+
+        return fitting
+
+    def get_crb(self,blast1,blast2,exclude_pairs=set()):
+        pairs = set()
+        for qname,hit_group in blast1.items():
+            for hit1 in hit_group.hits:
+                if self.fitting1.check(hit1) and hit1.sname in blast2:
+                    for hit2 in blast2[hit1.sname].hits:
+                        if hit2.sname == qname and  self.fitting2.check(hit2):
+                            pair = HitPair(hit1,hit2)
+                            if pair not in exclude_pairs and pair not in pairs:
+                                pairs.add(pair)
+                                #print pair
+        return pairs
+
+
+class RBH(object):
+    def __init__(self):
+        self.parse_args()
+        self.blast1 = self.qualifying_hits(self.args.BLAST_FILE1)
+        self.blast2 = self.qualifying_hits(self.args.BLAST_FILE2)
+
+    def run(self):
+        reciprocal_hits = set()
+        for qname in sorted(self.blast1.keys()):
+            hits = self.blast1[qname]
+            for winner in hits.winners:
+                rname = self.process_name(winner.sname)
+                if winner.sname in self.blast2:
+                    for rwinner in self.blast2[rname].winners:
+                        if self.process_name(rwinner.sname) == qname:
+                            reciprocal_hits.add(HitPair(winner,rwinner))
+        if self.args.crb:
+            crb_finder = CRB(reciprocal_hits)
+            crb_pairs = crb_finder.get_crb(self.blast1,self.blast2,reciprocal_hits)
+        orthologs = collections.defaultdict(list)
+        for pair in crb_pairs.union(reciprocal_hits):
+            orthologs[pair.hit1.qname].append(pair.hit2.qname)
+
+        for qname,hits in orthologs.items():
+            print '%s: %s' % (qname,','.join(hits))
+
+        #print '\n'.join(str(x) for x in reciprocal_hits)
+        return 
+
+    def parse_args(self):
+        parser = argparse.ArgumentParser(
+            description='''Find reciprocal best BLAST hits based on BLAST 
+            output of queries whole proteomes. Expected inputs are NCBI-BLAST
+            tables. For best results, use the option -outfmt "6 std qcov qlen
+            slen"''')
+        parser.add_argument('BLAST_FILE1')
+        parser.add_argument('BLAST_FILE2')
+        parser.add_argument('--max-evalue',metavar='E',type=float,default=1e-5,
+                            help='maximum evalue per hit [default: 1e-5]')
+        parser.add_argument('--min-qcov',metavar='C',type=float,default=0.,
+                            help='minimum query coverage per hit [default: 0]')
+        parser.add_argument('--crb',action='store_true',
+                            help='Add conditional best reciprocal blast hits')
+        # XXX: this does not increase RBH and is not implemented in CRB so 
+        # for now we will exclude this
+        #parser.add_argument('--trinity-gene-prefix',metavar='PREFIX',
+                            #help='''assume genes which begin with PREFIX are
+                            #trinity genes, and trim the isoform ID off when
+                            #searching for reciprocal hits.''')
+        self.args = parser.parse_args()
+
+    def parse_hit_table(self, file):
+        for lineno,line in enumerate(open(file)):
+            if line.startswith('#'):
+                continue
+            fields = line.split()
+            if len(fields) < 12 or len(fields) > 13:
+                raise ValueError, 'Could not parse BLAST hit on line %d of %s' % (
+                    lineno,file)
+            yield Hit(*fields)
+
+    def qualifying_hit(self, hit):
+        if hit.evalue <= self.args.max_evalue and \
+           hit.qcov >= self.args.min_qcov:
+            return True
+
+    def process_name(self,name):
+        #if self.args.trinity_gene_prefix is not None \
+           #and name.startswith(self.args.trinity_gene_prefix):
+            #return '_'.join(name.split('_')[:-1])
+        #else:
+        return name
+
+    def qualifying_hits(self,file):
+        hits = collections.defaultdict(HitGroup)
+        for hit in self.parse_hit_table(file):
+            if self.qualifying_hit(hit):
+                hits[self.process_name(hit.qname)].add(hit)
+        return hits
+
+
+if __name__ == '__main__':
+    rbh = RBH()
+    rbh.run()
