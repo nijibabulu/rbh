651c6103263b65d4d9a65c40790f28d16e3e69d8
Bob Zimmermann <robert.zimmermann@univie.ac.at>
2017-07-20 23:37:23 +0200

    major revisions to guarantee reciprocal and conditional orthologs are symmetric; change of output format and required arguments

--- rbh.py
+++ rbh.py
@@ -89,7 +89,8 @@ class HitPair(object):
 class CRBFitting(dict):
     def check(self,hit):
         length = min(hit.length,len(self)-1)
-        return self[length] <= CRB.transform_evalue(hit.bitscore)
+        score = CRB.transform_evalue(hit.bitscore)
+        return self[length] <= score
     
 class CRB(object):
     MIN_EVALUE=sys.float_info.min
@@ -98,10 +99,11 @@ class CRB(object):
                              itertools.chain((pair.hit1 for pair in rbh),
                                              (pair.hit2 for pair in rbh))
                              if hit.evalue > 0.0)
-        self.fitting1 = self.get_fitting([pair.hit1 for pair in rbh],
-                                         output_training,output_fitting_data,output_fitting)
-        self.fitting2 = self.get_fitting([pair.hit2 for pair in rbh])#,
-                                         #output_training,output_fitting)
+        self.fitting1 = self.get_fitting(
+            set(pair.hit1 for pair in rbh),
+            output_training,output_fitting_data,output_fitting)
+        self.fitting2 = self.get_fitting(
+            set(pair.hit2 for pair in rbh))
     
     @classmethod
     def transform_evalue(cls,evalue):
@@ -147,44 +149,113 @@ class CRB(object):
 
         return fitting
 
+    def _get_crb_pairs(self,blastA,blastB,fittingA,fittingB,exclude_pairs,pair_fac):
+        log = lambda x: sys.stderr.write(x)
+        pairs = []
+        for qname,hit_group in blastA.items():
+            for hitA in hit_group.hits:
+                if fittingA.check(hitA) and hitA.sname in blastB:
+                    for hitB in blastB[hitA.sname].hits:
+                        if hitB.sname == qname and fittingB.check(hitB):
+                            pair = pair_fac(hitA,hitB)
+                            if pair not in exclude_pairs:
+                                pairs.append(pair)
+        return set(pairs)
+
+
     def get_crb(self,blast1,blast2,exclude_pairs=set()):
-        pairs = set()
-        for qname,hit_group in blast1.items():
-            for hit1 in hit_group.hits:
-                if self.fitting1.check(hit1) and hit1.sname in blast2:
-                    for hit2 in blast2[hit1.sname].hits:
-                        if hit2.sname == qname and  self.fitting2.check(hit2):
-                            pair = HitPair(hit1,hit2)
-                            if pair not in exclude_pairs and pair not in pairs:
-                                pairs.add(pair)
+        pairs = self._get_crb_pairs(
+            blast1,blast2,
+            self.fitting1,self.fitting2,
+            exclude_pairs,lambda a,b: HitPair(a,b)
+        ).union(self._get_crb_pairs(
+            blast2,blast1,
+            self.fitting2,self.fitting1,
+            exclude_pairs,lambda a,b: HitPair(b,a)))
         return pairs
 
-class HitList(object):
-    def __init__(self):
-        self.reciprocal_pair = None
-        self.crb_pairs = []
-    def _get_query(self):
-        if self.reciprocal_pair is not None:
-            return self.reciprocal_pair.hit1.qname
-        elif len(self.crb_pairs):
-            return self.crb_pairs[0].hit1.qname
-        else:
-            return ''
-    query = property(_get_query)
-
+class OrthologyGroup(object):
+    def __init__(self,species_a_orthologs=[],species_b_orthologs=[]):
+        self.a = set(species_a_orthologs)
+        self.b = set(species_b_orthologs)
+    def add_orthologs(self,species_a_orthologs=[],species_b_orthologs=[]):
+        self.a = self.a.union(set(species_a_orthologs))
+        self.b = self.b.union(set(species_b_orthologs))
+    def add_species_a_orthologs(self,orthologs):
+        self.add_orthologs(species_a_orthologs=orthologs)
+    def add_species_b_orthologs(self,orthologs):
+        self.add_orthologs(species_b_orthologs=orthologs)
+    def update(self,other_orthology_group):
+        self.add_orthologs(other_orthology_group.a,other_orthology_group.b)
+    def difference_update(self,other):
+        self.a.difference_update(other.a)
+        self.b.difference_update(other.b)
+    def intersect(self,species_a_orthologs=None,species_b_orthologs=None):
+        if species_a_orthologs is not None:
+            self.a = self.a.intersection(set(species_a_orthologs))
+        if species_b_orthologs is not None:
+            self.b = self.b.intersection(set(species_b_orthologs))
+    def has_single_linkage(self,other):
+        if not self.a.isdisjoint(other.a) or not self.b.isdisjoint(other.b):
+            return True
+        return False
     def all(self):
-        if self.reciprocal_pair is not None:
-            return [self.reciprocal_pair] + self.crb_pairs
-        else:
-            return self.crb_pairs
-
+        return set(self.a).union(self.b)
     def __str__(self):
-        result = '%s\t' % self.query
-        if self.reciprocal_pair is not None:
-            result += self.reciprocal_pair.hit1.sname
-        if len(self.crb_pairs):
-            result += '\t' + '\t'.join(pair.hit1.sname for pair in self.crb_pairs)
-        return result
+        return 'a:%s,b:%s' % (' '.join(self.a), ' '.join(self.b))
+    def __len__(self):
+        return len(self.a) + len(self.b)
+
+class ReciprocalOrthologyGroup(object):
+    def __init__(self):
+        self.reciprocal_hits = set()
+        self.reciprocal_group = OrthologyGroup()
+        self.crb_group = OrthologyGroup()
+    def add_reciprocal_pair(self,hit1,hit2):
+        self.reciprocal_group.add_orthologs([hit1.qname],[hit2.qname])
+        self.reciprocal_hits.add(HitPair(hit1,hit2))
+    def add_crb_pair(self,hit1,hit2):
+        if hit1.qname not in self.reciprocal_group.a:
+            self.crb_group.a.add(hit1.qname)
+        if hit2.qname not in self.reciprocal_group.b:
+            self.crb_group.b.add(hit2.qname)
+    def intersect(self,species_a_orthologs=None,species_b_orthologs=None):
+        self.reciprocal_group.intersect(species_a_orthologs,species_b_orthologs)
+        self.crb_group.intersect(species_a_orthologs,species_b_orthologs)
+    def all(self):
+        return self.reciprocal_group.all().union(self.crb_group.all())
+    def __str__(self):
+        return '%s\t%s\t%s\t%s' % (
+            ' '.join(sorted(self.reciprocal_group.a)),
+            ' '.join(sorted(self.crb_group.a)),
+            ' '.join(sorted(self.reciprocal_group.b)),
+            ' '.join(sorted(self.crb_group.b))
+        )
+
+class SpeciesStats(object):
+    def __init__(self,species_name):
+        self.species_name = species_name
+        self.total_genes = set()
+        self.rbh_genes = set()
+        self.crb_genes = set()
+
+class RBHSummary(object):
+    def __init__(self,species_a,species_b,target_list_a=None,target_list_b=None):
+        self.a = SpeciesStats(species_a)
+        self.b = SpeciesStats(species_b)
+    def summary_header(self):
+        return '\t%s\t%s\n' % (self.a.species_name,self.b.species_name)
+    def summary_table(self,prefix=''):
+        return '%sGenes\t%d\t%d\n%sRBB Genes\t%d\t%d\n%sCRB Genes\t%d\t%d\n' % (
+            prefix,len(self.a.total_genes),len(self.b.total_genes),
+            prefix,len(self.a.rbh_genes),len(self.b.rbh_genes),
+            prefix,len(self.a.crb_genes),len(self.b.crb_genes))
+
+class RBHArgumentParser(argparse.ArgumentParser):
+    def error(self,message):
+        sys.stderr.write('error: %s\n' % message)
+        self.print_help()
+        sys.exit(2)
 
 class RBH(object):
     def __init__(self):
@@ -192,8 +263,19 @@ class RBH(object):
         self.blast1 = self.qualifying_hits(self.args.BLAST_FILE1)
         self.blast2 = self.qualifying_hits(self.args.BLAST_FILE2)
 
-    def run(self):
-        orthologs = collections.defaultdict(HitList)
+    def _get_summary(self,ortholog_set,total_genes1,total_genes2):
+        summary = RBHSummary(self.args.SPECIES_NAME1,self.args.SPECIES_NAME2)
+        summary.a.total_genes.update(total_genes1)
+        summary.b.total_genes.update(total_genes2)
+        for ortholog in ortholog_set:
+            summary.a.rbh_genes.update(ortholog.reciprocal_group.a)
+            summary.b.rbh_genes.update(ortholog.reciprocal_group.b)
+            summary.a.crb_genes.update(ortholog.crb_group.a)
+            summary.b.crb_genes.update(ortholog.crb_group.b)
+        return summary
+
+    def _get_rbh(self):
+        orthologs = collections.defaultdict(ReciprocalOrthologyGroup)
         for qname in sorted(self.blast1.keys()):
             hits = self.blast1[qname]
             for winner in hits.winners:
@@ -201,64 +283,128 @@ class RBH(object):
                 if winner.sname in self.blast2:
                     for rwinner in self.blast2[rname].winners:
                         if self.process_name(rwinner.sname) == qname:
-                            orthologs[qname].reciprocal_pair = HitPair(winner,rwinner)
+                            orthologs[qname].add_reciprocal_pair(winner,rwinner)
+
+        for q1,q2 in itertools.combinations(orthologs.keys(),r=2):
+            if orthologs[q1].reciprocal_group.has_single_linkage(
+                orthologs[q2].reciprocal_group):
+                orthologs[q1].reciprocal_hits.update(
+                    orthologs[q2].reciprocal_hits)
+                orthologs[q1].reciprocal_group.update(
+                    orthologs[q2].reciprocal_group)
+                orthologs[q2] = orthologs[q1]
+
+        return set(orthologs.values())
+
+    def _get_crb(self,ortholog_set):
+        reciprocal_hits = set(itertools.chain.from_iterable(
+            ortholog.reciprocal_hits for ortholog in ortholog_set))
+            
+        crb_finder = CRB(reciprocal_hits,
+                         self.args.output_evalue_table,
+                         self.args.output_fit_table,
+                         self.args.output_fit_values)
+        crb_pairs = crb_finder.get_crb(self.blast1,self.blast2,reciprocal_hits)
+
+        orthologs = {}
+        for ortholog in ortholog_set:
+            for name in ortholog.reciprocal_group.all():
+                orthologs[name] = ortholog
+
+        if self.args.output_summary:
+            crb1 = set()
+            crb2 = set()
+            for pair in crb_pairs:
+                crb1.add(pair.hit1.qname)
+                crb2.add(pair.hit2.qname)
+            self.args.output_summary.write('%d\t%d\n' % (len(crb1),len(crb2)))
+
+        for pair in crb_pairs:
+            if pair.hit1.qname in orthologs:
+                orthologs[pair.hit1.qname].add_crb_pair(pair.hit1,pair.hit2)
+            if pair.hit2.qname in orthologs:
+                orthologs[pair.hit2.qname].add_crb_pair(pair.hit1,pair.hit2)
+            if not any(hit.qname in orthologs for hit in [pair.hit1,pair.hit2]):
+                ortholog = ReciprocalOrthologyGroup()
+                ortholog.add_crb_pair(pair.hit1,pair.hit2)
+                orthologs[pair.hit1.qname] = ortholog
+                orthologs[pair.hit2.qname] = ortholog
+        return ortholog_set
+
+    def _filter_targets(self,ortholog_set):
+        if self.args.target_list1:
+            list1_orthologs = set(
+                group for group in ortholog_set 
+                if not group.all().isdisjoint(self.args.target_list1))
+        else:
+            list1_orthologs = set(ortholog_set)
+
+        if self.args.target_list2:
+            list2_orthologs = set(
+                group for group in ortholog_set 
+                if not group.all().isdisjoint(self.args.target_list2))
+        else:
+            list2_orthologs = dict(orthologs)
+
+        if self.args.target_list1 and self.args.target_list2:
+            if self.args.target_list_intersection:
+                ortholog_set = list1_orthologs.intersection(list2_orthologs)
+            else:
+                ortholog_set = list1_orthologs.union(list2_orthologs)
+        else:
+            # the one without the list is just the full list -- just take
+            # the intersection
+            ortholog_set = list1_orthologs.intersection(list2_orthologs)
+
+
+        if self.args.target_list_intersection:
+            if self.args.target_list1:
+                for group in ortholog_set:
+                    group.intersect(species_a_orthologs=self.args.target_list1)
+            if self.args.target_list2:
+                for group in ortholog_set:
+                    group.intersect(species_b_orthologs=self.args.target_list2)
+
+        return ortholog_set
+
+    def run(self):
+        ortholog_set = self._get_rbh()
 
         if self.args.crb:
-            reciprocal_hits = set(ortholog.reciprocal_pair for ortholog in orthologs.values())
-                
-            crb_finder = CRB(reciprocal_hits,
-                             self.args.output_evalue_table,
-                             self.args.output_fit_table,
-                             self.args.output_fit_values)
-            crb_pairs = crb_finder.get_crb(self.blast1,self.blast2,reciprocal_hits)
-            for pair in crb_pairs:
-                orthologs[pair.hit1.qname].crb_pairs.append(pair)
+            ortholog_set = self._get_crb(ortholog_set)
 
-        if self.args.target_list1 or self.args.target_list2:
-            if self.args.target_list1:
-                list1_orthologs = {q:hl for q,hl in orthologs.items() 
-                                   if q in self.args.target_list1}
-            else:
-                list1_orthologs = dict(orthologs)
-            if self.args.target_list2:
-                list2_orthologs = {q:hl for q,hl in orthologs.items()
-                                   if any(hit_pair.hit2.qname in self.args.target_list2
-                                          for hit_pair in hl.all()) }
-                # XXX: filtering out the list should be more elegant than this
-                for hit_list in list2_orthologs.values():
-                    if hit_list.reciprocal_pair is not None and \
-                       hit_list.reciprocal_pair.hit2.qname \
-                       not in self.args.target_list2:
-                        hit_list.reciprocal_pair = None
-                    hit_list.crb_pairs = [
-                        hit_pair for hit_pair in hit_list.crb_pairs
-                        if hit_pair.hit2.qname in self.args.target_list2]
-            else:
-                list2_orthologs = dict(orthologs)
-            #print len(list1_orthologs), len(list2_orthologs)
-            # XXX: This should also be cleaned up!
-            if self.args.target_list1 and self.args.target_list2:
-                if self.args.target_list_intersection:
-                    ortholog_set = set(list1_orthologs.values()).intersection(
-                        list2_orthologs.values())
-                else:
-                    ortholog_set = set(list1_orthologs.values()).union(
-                        list2_orthologs.values())
-            else:
-                ortholog_set = set(list1_orthologs.values()).intersection(
-                        list2_orthologs.values())
-        else:
-            ortholog_set = set(orthologs.values())
+        if self.args.output_summary:
+            summary_prefilter = self._get_summary(
+                ortholog_set, self.blast1.keys(), self.blast2.keys())
+            self.args.output_summary.write(summary_prefilter.summary_header())
+            self.args.output_summary.write(summary_prefilter.summary_table())
             
-        print '\n'.join(str(x) for x in ortholog_set)
+        if self.args.target_list1 or self.args.target_list2:
+            ortholog_set = self._filter_targets(ortholog_set)
+
+            if self.args.output_summary:
+                summary_postfilter = self._get_summary(
+                    ortholog_set,self.args.target_list1,self.args.target_list2)
+                self.args.output_summary.write(
+                    summary_postfilter.summary_table(prefix='Target '))
+
+         
+        if not self.args.silent:
+            if not self.args.no_header:
+                print '%s RBB Genes\t%s CRB Genes\t%s RBB Genes\t%s CRB Genes' % (
+                    self.args.SPECIES_NAME1,self.args.SPECIES_NAME1,
+                    self.args.SPECIES_NAME2,self.args.SPECIES_NAME2)
+            print '\n'.join(str(x) for x in ortholog_set)
         return 
 
     def parse_args(self):
-        parser = argparse.ArgumentParser(
+        parser = RBHArgumentParser(
             description='''Find reciprocal best BLAST hits based on BLAST 
             output of queries whole proteomes. Expected inputs are NCBI-BLAST
             tables.''')
-        listfile = lambda fn: [l.strip() for l in open(fn)]
+        setfile = lambda fn: set([l.strip() for l in open(fn)])
+        parser.add_argument('SPECIES_NAME1')
+        parser.add_argument('SPECIES_NAME2')
         parser.add_argument('BLAST_FILE1')
         parser.add_argument('BLAST_FILE2')
         parser.add_argument('--max-evalue',metavar='E',type=float,default=1e-5,
@@ -271,23 +417,32 @@ class RBH(object):
                             help='''Report the intersection of the
                             riciprocal/conditional target hits lists rather 
                             than the union''')
-        parser.add_argument('--target-list1',metavar='LIST',type=listfile,
+        parser.add_argument('--target-list1',metavar='LIST',type=setfile,
                            help='''Output hits containing queries from
                            BLAST_FILE1 in this list''')
-        parser.add_argument('--target-list2',metavar='LIST',type=listfile,
+        parser.add_argument('--target-list2',metavar='LIST',type=setfile,
                            help='''Output hits containing queries from
                            BLAST_FILE2 in this list''')
-        parser.add_argument('--output-evalue-table',metavar='TAB',
-                            type=argparse.FileType('w'),help='''Output all
-                            evalue and length values used for training the CRB
+        parser.add_argument('--output-summary',metavar='FILE',
+                            type=argparse.FileType('w'),help='''Output summary
+                            statistics for the number of RBB and CRB genes in a
+                            tab-delimited table''')
+        parser.add_argument('--silent',action='store_true',help='''Do not output
                             hits''')
+        parser.add_argument('--no-header',action='store_true',help='''Do not
+                            inlcude a header line in the output''')
+
+        # hidden arguments
+        '''Output all evalue and length values used for training the CRB hits'''
+        parser.add_argument('--output-evalue-table',metavar='TAB',
+                            type=argparse.FileType('w'),help=argparse.SUPPRESS)
+        '''Output all training data including data re-binned into neighboring 
+        length categories.'''
         parser.add_argument('--output-fit-table',metavar='TAB',
-                            type=argparse.FileType('w'),help='''Output all
-                            training data including data re-binned into
-                            neighboring length categories.''')
+                            type=argparse.FileType('w'),help=argparse.SUPPRESS)
+        '''Output fitting function'''
         parser.add_argument('--output-fit-values',metavar='VAL',
-                            type=argparse.FileType('w'),help='''Output fitting
-                            function''')
+                            type=argparse.FileType('w'),help=argparse.SUPPRESS)
 
         # XXX: this does not increase RBH and is not implemented in CRB so 
         # for now we will exclude this
